edb7272e0f02110429a852841ea195e92f4d8b12
Radhey Kalra <radheykalra901@gmail.com>
2025-09-15 13:46:13 +0530

    Implement file handler

---
+++ backend/internal/handler/file.go
@@ -0,0 +1,34 @@
+﻿package handler
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/homelab/filemanager/internal/service"
+)
+
+type FileHandler struct {
+	fileService service.FileService
+}
+
+func NewFileHandler(fileService service.FileService) *FileHandler {
+	return &FileHandler{fileService: fileService}
+}
+
+func (h *FileHandler) RegisterRoutes(r chi.Router) {
+	r.Get("/*", h.ListFiles)
+}
+
+func (h *FileHandler) ListFiles(w http.ResponseWriter, r *http.Request) {
+	path := chi.URLParam(r, "*")
+	
+	files, err := h.fileService.List(r.Context(), path)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(files)
+}
